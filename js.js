59e2edde2960297fb55e586e8e4a474ff29b0453
Harish A <143674041+Harishviral3@users.noreply.github.com>
2025-02-28 20:49:10 +0530

    landing page interaction interface

---
+++ js.js
@@ -0,0 +1,10 @@
+function menuClick () {
+    var menu = document.getElementById('mobileMenu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    }
+    else {
+        menu.style.display = 'block';
+    }
+
+}
